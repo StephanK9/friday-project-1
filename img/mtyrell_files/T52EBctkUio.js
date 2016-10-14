baba90a3108df9561cfe3f8612339773a6bdc20f
Stephan Khoury <stephane.khoury@gmail.com>
2016-10-14 15:47:34 -0700

    add pretty much everything we need for today, last pictures, last text, (almost final format).

---
+++ img/mtyrell_files/T52EBctkUio.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["qhNcw"]); }
+
+__d("XPrivacyPromotionTriggerRecordingController",["XController"],(function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports=c("XController").create("\/privacy\/promotion_triggers\/record\/",{});}),null);
